f740a67efa8706ea9d6b53f66c65a77250a9c888
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-01-30 13:47:53 -0500

    Add files via upload

---
+++ homework 2 q 1.py
@@ -0,0 +1,31 @@
+a = 'Linus Benedict Torvalds, (317)-555-1969'
+b = 'Alan Mathison Turing, (317)-555-5346'
+c = 'Stephan Gary Wozniak, (317)-555-3720'
+d = 'Adele Goldberg, (317)-555-2345'
+
+print("user name a:")
+print(a[14:21]+a[0]+a[6])
+
+print("password a:")
+print(a[0:2]+a[6]+a[8]+a[10]+a[35:39])
+print("\n")
+
+print("user name b:")
+print(b[13:20]+b[0]+b[5])
+
+print("password b:")
+print(b[0:2]+b[6]+b[8]+b[10]+b[32:36])
+print("\n")
+
+print("username c:")
+print(c[12:19]+c[0]+c[8])
+
+print("password c:")
+print(c[0:2]+c[8]+c[10]+c[32:36])
+print("\n")
+
+print("user name d:")
+print(d[6:12]+d[0])
+
+print("password d:")
+print(d[0:2]+d[26:30])
---
+++ homework 2 q 2.py
@@ -0,0 +1,9 @@
+a = 'simple'
+b = 'friends'
+c = 'omelet'
+d = 'ions'
+
+print(a[1:6]+a[-6]+'ay')
+print(b[1:7]+b[-7]+'ay')
+print(c[2:6]+c[0:1]+c[1]+'ay')
+print(d[3]+d[0:3]+'ay')
